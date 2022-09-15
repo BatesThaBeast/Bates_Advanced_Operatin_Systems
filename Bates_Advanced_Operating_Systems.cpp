fb10954e9712ad0960f983f1c369e44e0e17e9ad
BatesThaBeast <58051984+BatesThaBeast@users.noreply.github.com>
2022-09-15 12:50:07 -0700

    Changes to main
    Added some commands to show that the passworldfile class is working

--- Bates_Advanced_Operating_Systems.cpp
+++ Bates_Advanced_Operating_Systems.cpp
@@ -2,11 +2,24 @@
 //
 
 #include "Bates_Advanced_Operating_Systems.h"
+#include "passwordfile.h"
+#include "sdisk.h"
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello CMake." << endl;
+	PasswordFile passfile("password.txt");
+	passfile.addpw("dbotting", "123qwe");//adding user
+	passfile.printusr();//checking the vectors
+	passfile.addpw("egomez", "qwerty");//adding user
+	passfile.printusr();//checking the vectors
+	passfile.addpw("tongyu", "liberty");//adding user
+	passfile.printusr();//checking the vectors
+	passfile.addpw("dbotting", "456ewq");//test to see if you can have 2 users with the same name
+	passfile.printusr();//checking the vectors
+	passfile.delusr("dbotting");
+	passfile.printusr();//checking the vectors
+	// write some lines to see if passwords match users
 	return 0;
 }

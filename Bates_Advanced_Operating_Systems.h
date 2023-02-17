8ab8f6db6371bb63a8291f77b3fd26858c11263d
BatesThaBeast <58051984+BatesThaBeast@users.noreply.github.com>
2023-02-17 09:30:55 -0800

    Finished project
    Final classes added; Shell, Table

--- Bates_Advanced_Operating_Systems.h
+++
@@ -1,8 +0,0 @@
-﻿// Bates_Advanced_Operatin_Systems.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-
-// TODO: Reference additional headers your program requires here.
